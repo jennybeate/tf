bd75f81222f50c9e28ef825c227877da57bbbbbd
jenny@nimtech.no <jenny@nimtech.no>
2026-04-15 01:52:42 -0700

    fix: kubernetes module v2 fixes

--- infra-as-code/terraform/modules/kubernetes/v2.0.0/data.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_user_assigned_identity" "identity" {
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  name                = local.identity_name
-}
