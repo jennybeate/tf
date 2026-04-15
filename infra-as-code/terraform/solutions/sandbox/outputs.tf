68d956d3de8719cff8a33bf08f0b285aafc37786
jenny@nimtech.no <jenny@nimtech.no>
2026-04-15 02:22:32 -0700

    fix: rm stg output

--- infra-as-code/terraform/solutions/sandbox/outputs.tf
+++ infra-as-code/terraform/solutions/sandbox/outputs.tf
@@ -23,18 +23,3 @@ output "resource_group_name" {
   value       = azurerm_resource_group.main.name
 }
 
-output "storage_account_id" {
-  description = "The ID of the storage account."
-  value       = module.storage_account.storage_account_id
-}
-
-output "storage_account_name" {
-  description = "The name of the storage account."
-  value       = module.storage_account.storage_account_name
-}
-
-output "storage_account_primary_connection_string" {
-  description = "The primary connection string for the storage account."
-  value       = module.storage_account.storage_account_primary_connection_string
-  sensitive   = true
-}
