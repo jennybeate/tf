a5465ff17ff527dc232ba20c83c1fddc99a427ca
jenny@nimtech.no <jenny@nimtech.no>
2026-04-16 00:37:50 -0700

    fix: uami outputs

--- infra-as-code/terraform/modules/kubernetes/v2.0.0/outputs.tf
+++ infra-as-code/terraform/modules/kubernetes/v2.0.0/outputs.tf
@@ -10,17 +10,17 @@ output "cluster_name" {
 
 output "identity_client_id" {
   description = "The client ID of the user-assigned managed identity."
-  value       = module.user_assigned_identity.identity.client_id
+  value       = module.user_assigned_identity.client_id
 }
 
 output "identity_id" {
   description = "The resource ID of the user-assigned managed identity."
-  value       = module.user_assigned_identity.identity.resource_id
+  value       = module.user_assigned_identity.resource_id
 }
 
 output "identity_principal_id" {
   description = "The principal ID of the user-assigned managed identity. Used for RBAC assignments."
-  value       = module.user_assigned_identity.identity.principal_id
+  value       = module.user_assigned_identity.principal_id
 }
 
 output "kube_config_raw" {
