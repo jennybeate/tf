042706386b1318ac99fdf1f7d351c20a6c9eb9e1
Jenny Veddeng <jenny_veddeng@hotmail.com>
2026-05-13 12:21:22 +0200

    fix: remove imports (#26)
    * fix: remove imports
    
    * fix: remove current
    
    ---------
    
    Co-authored-by: jenny@nimtech.no <jenny@nimtech.no>

--- infra-as-code/terraform/solutions/sandbox/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = module.user_assigned_identity.azurerm_user_assigned_identity.this
-  id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.ManagedIdentity/userAssignedIdentities/${local.identity_name}"
-}
-
-import {
-  to = module.aks.azapi_resource.this
-  id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${local.resource_group_name}/providers/Microsoft.ContainerService/managedClusters/${local.aks_name}"
-}
