5e7e34e53f848d2640ec1c976f77db05a7077164
jenny@nimtech.no <jenny@nimtech.no>
2026-05-13 10:22:07 +0200

    fix: environment

--- infra-as-code/terraform/solutions/sandbox-gitops/variables.tf
+++ infra-as-code/terraform/solutions/sandbox-gitops/variables.tf
@@ -8,16 +8,6 @@ variable "cluster_resource_group" {
   description = "Resource group containing the AKS cluster."
 }
 
-variable "environment" {
-  type        = string
-  description = "Deployment environment code."
-  validation {
-    condition     = contains(["can", "liv", "dev", "sbx", "tst", "uat", "stg", "prod"], var.environment)
-    error_message = "Must be one of: can, liv, dev, sbx, tst, uat, stg, prod."
-  }
-  default = "sbx"
-}
-
 variable "argocd_chart_version" {
   type        = string
   description = "Argo CD Helm chart version. Chart 7.x installs Argo CD 2.13.x."
