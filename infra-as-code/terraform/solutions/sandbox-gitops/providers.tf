b25f2acadecbbd02c0d7a1b8e26d38fcb6401400
jenny@nimtech.no <jenny@nimtech.no>
2026-05-13 14:06:57 +0200

    fix: kubelogin installments etc

--- infra-as-code/terraform/solutions/sandbox-gitops/providers.tf
+++ infra-as-code/terraform/solutions/sandbox-gitops/providers.tf
@@ -3,10 +3,13 @@ provider "azurerm" {
 }
 
 provider "helm" {
-  kubernetes  {
+  kubernetes {
     host                   = data.azurerm_kubernetes_cluster.main.kube_config[0].host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.main.kube_config[0].client_key)
     cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "kubelogin"
+      args        = ["get-token", "--login", "workloadidentity", "--server-id", "6dae42f8-4368-4678-94ff-3960e28e3630"]
+    }
   }
 }
